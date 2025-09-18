2ca78a324bfb541ab53396afee21c8f5586b0c7d
j0hnr0 <johnrovanvcabucos@gmail.com>
2025-09-18 22:47:31 +0800

    added instrument-sans font and colors

--- app/layout.js
+++ app/layout.js
@@ -1,14 +1,39 @@
 import "./globals.css";
+import localFont from "next/font/local";
 
 export const metadata = {
   title: "Link Sharing App",
   description: "Share your links to your friend.",
 };
 
+const instrumentSans = localFont({
+  src: [
+    {
+      path: "../public/fonts/static/InstrumentSans-Regular.ttf",
+      weight: "400",
+      style: "normal",
+    },
+    {
+      path: "../public/fonts/static/InstrumentSans-SemiBold.ttf",
+      weight: "600",
+      style: "normal",
+    },
+    {
+      path: "../public/fonts/static/InstrumentSans-Bold.ttf",
+      weight: "700",
+      style: "normal",
+    },
+  ],
+  variable: "--font-instrument-sans",
+  display: "swap",
+});
+
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body className="antialiased">{children}</body>
+      <body className={`${instrumentSans.variable} antialiased`}>
+        {children}
+      </body>
     </html>
   );
 }
--- app/page.js
+++ app/page.js
@@ -13,9 +13,28 @@ export default function Home() {
         />
 
         <form className="bg-white rounded-xl p-10">
-          <h1 className="text-custom-grey-900 text-[32px] font-bold">
+          <h1 className="instrument-sans text-custom-grey-900 text-[32px] font-bold">
             Login
           </h1>
+          <p className="instrument-sans font-normal text-base text-custom-grey-500">
+            Add your details below to get back into the app
+          </p>
+
+          <div className="mt-10">
+            <label className="instrument-sans font-normal text-xs text-custom-grey-900">
+              Email Address
+            </label>
+
+            <div className="mt-2 w-[396px] border border-custom-grey-200 rounded-lg p-4 flex justify-start items-center gap-4">
+              {/* eslint-disable-next-line @next/next/no-img-element */}
+              <img
+                src="/images/icon-email.svg"
+                alt="Email Icon"
+                width={16}
+                height={16}
+              />
+            </div>
+          </div>
         </form>
       </div>
     </main>
