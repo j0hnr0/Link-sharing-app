b419575169eef44c1812e15a3fd321ecadfd63ea
j0hnr0 <johnrovanvcabucos@gmail.com>
2025-09-19 22:07:36 +0800

    desktop form ui done

--- app/page.js
+++ app/page.js
@@ -1,4 +1,5 @@
 import Image from "next/image";
+import Link from "next/link";
 
 export default function Home() {
   return (
@@ -12,72 +13,88 @@ export default function Home() {
           priority
         />
 
-        <form className="bg-white rounded-xl p-10">
-          <h1 className="instrument-sans text-custom-grey-900 text-[32px] font-bold">
-            Login
-          </h1>
-          <p className="instrument-sans font-normal text-base text-custom-grey-500">
-            Add your details below to get back into the app
-          </p>
+        <div className="bg-white rounded-xl p-10">
+          <form>
+            <h1 className="instrument-sans text-custom-grey-900 text-[32px] font-bold">
+              Login
+            </h1>
+            <p className="instrument-sans font-normal text-base text-custom-grey-500">
+              Add your details below to get back into the app
+            </p>
 
-          <div className="mt-10">
-            <label
-              htmlFor="email"
-              className="instrument-sans font-normal text-xs text-custom-grey-900"
-            >
-              Email Address
-            </label>
+            <div className="mt-10">
+              <label
+                htmlFor="email"
+                className="instrument-sans font-normal text-xs text-custom-grey-900"
+              >
+                Email Address
+              </label>
 
-            <div className="mt-2 w-[396px] border border-custom-grey-200 rounded-lg p-4 flex justify-start items-center gap-4">
-              {/* eslint-disable-next-line @next/next/no-img-element */}
-              <img
-                src="/images/icon-email.svg"
-                alt="Email Icon"
-                width={16}
-                height={16}
-              />
+              <div className="autocomplete-highlight mt-2 w-[396px] border border-custom-grey-200 rounded-lg p-4 flex justify-start items-center gap-4">
+                {/* eslint-disable-next-line @next/next/no-img-element */}
+                <img
+                  src="/images/icon-email.svg"
+                  alt="Email Icon"
+                  width={16}
+                  height={16}
+                />
 
-              <input
-                type="email"
-                name="email"
-                id="email"
-                className="instrument-sans font-normal text-base text-custom-grey-900 w-full h-full focus:outline-none"
-                placeholder="e.g. alex@email.com"
-              />
+                <input
+                  type="email"
+                  name="email"
+                  id="email"
+                  className="instrument-sans font-normal text-base text-custom-grey-900 w-full h-full focus:outline-none"
+                  placeholder="e.g. alex@email.com"
+                />
+              </div>
             </div>
-          </div>
 
-          <div className="mt-6">
-            <label
-              htmlFor="password"
-              className="instrument-sans font-normal text-xs text-custom-grey-900"
-            >
-              Password
-            </label>
+            <div className="mt-6">
+              <label
+                htmlFor="password"
+                className="instrument-sans font-normal text-xs text-custom-grey-900"
+              >
+                Password
+              </label>
 
-            <div className="mt-2 w-[396px] border border-custom-grey-200 rounded-lg p-4 flex justify-start items-center gap-4">
-              {/* eslint-disable-next-line @next/next/no-img-element */}
-              <img
-                src="/images/icon-password.svg"
-                alt="Email Icon"
-                width={16}
-                height={16}
-              />
+              <div className="autocomplete-highlight mt-2 w-[396px] border border-custom-grey-200 rounded-lg p-4 flex justify-start items-center gap-4">
+                {/* eslint-disable-next-line @next/next/no-img-element */}
+                <img
+                  src="/images/icon-password.svg"
+                  alt="Email Icon"
+                  width={16}
+                  height={16}
+                />
 
-              <input
-                type="password"
-                name="password"
-                id="password"
-                className="instrument-sans font-normal text-base text-custom-grey-900 w-full h-full focus:outline-none"
-                placeholder="Enter your password"
-              />
+                <input
+                  type="password"
+                  name="password"
+                  id="password"
+                  className="instrument-sans font-normal text-base text-custom-grey-900 w-full h-full focus:outline-none"
+                  placeholder="Enter your password"
+                />
+              </div>
             </div>
-          </div>
 
-          <button type="button" className="mt-6 w-full rounded-lg py-4 text-center bg-custom-purple-600">
-            
-          </button>
-        </form>
+            <button
+              type="button"
+              className="mt-6 w-full rounded-lg py-4 text-center bg-custom-purple-600"
+            >
+              <span className="instrument-sans text-base font-semibold text-white">
+                Login
+              </span>
+            </button>
+          </form>
+
+          <div className="mt-6 text-center">
+            <p className="instrument-sans font-normal text-base text-custom-grey-500">
+              Don&apos;t have an account?{" "}
+              <span className="text-custom-purple-600">
+                <Link href="/">Create account</Link>
+              </span>
+            </p>
+          </div>
+        </div>
       </div>
     </main>
   );
