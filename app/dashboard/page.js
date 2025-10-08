76c5f2c0a773c89d98ee0adbeeceb903037e9d29
j0hnr0 <johnrovanvcabucos@gmail.com>
2025-10-08 22:03:59 +0800

    continuation of customize your links part

--- app/dashboard/page.js
+++ app/dashboard/page.js
@@ -23,6 +23,41 @@ export default function Dashboard() {
           <h1 className="instrument-sans font-bold text-[32px] text-custom-grey-900">
             Customize your links
           </h1>
+          <p className="mt-2 instrument-sans font-normal text-base text-custom-grey-500">
+            Add/edit/remove links below and then share all your profiles with
+            the world!
+          </p>
+
+          <button
+            type="button"
+            className="mt-10 w-full rounded-lg border border-custom-purple-600 py-4 text-center cursor-pointer"
+          >
+            <span className="instrument-sans font-semibold text-base text-custom-purple-600">
+              + Add new link
+            </span>
+          </button>
+
+          <div className="mt-6 w-full rounded-xl bg-custom-grey-50 py-[67px]">
+            <Image
+              src="/images/illustration-empty.svg"
+              className="mx-auto"
+              width={249}
+              height={160}
+              alt="A hand clicking on a phone"
+            />
+
+            <h1 className="mt-10 text-center instrument-sans font-bold text-[32px] text-custom-grey-900">
+              Let&apos;s get you started
+            </h1>
+
+            <div className="mt-6 max-w-[488px] mx-auto text-center">
+              <p className="instrument-sans font-normal text-base text-custom-grey-500">
+                Use the “Add new link” button to get started. Once you have more
+                than one link, you can reorder and edit them. We’re here to help
+                you share your profiles with everyone!
+              </p>
+            </div>
+          </div>
         </div>
       </div>
     </main>
