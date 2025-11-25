249652bafbb0a0fc7e6c5e99e883c1ab4e0ba78c
j0hnr0 <johnrovanvcabucos@gmail.com>
2025-11-25 15:40:55 +0800

    api auth credentials

---
+++ app/api/auth/[...nextauth]/route.js
@@ -0,0 +1,18 @@
+import CredentialsProvider from "next-auth/providers/credentials";
+
+export const authOptions = {
+  providers: [
+    CredentialsProvider({
+      name: "Credentials",
+      credentials: {
+        email: { label: "Email", type: "email" },
+        password: { label: "Password", type: "password" },
+      },
+      async authorize(credentials) {
+        if (!credentials?.email || !credentials?.password) {
+          return null;
+        }
+      },
+    }),
+  ],
+};
