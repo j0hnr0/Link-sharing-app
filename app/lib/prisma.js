28caf9e02757fc8f7ca1307049b45595d8ae95f8
j0hnr0 <johnrovanvcabucos@gmail.com>
2025-12-23 16:17:59 +0800

    revert changes

--- app/lib/prisma.js
+++ app/lib/prisma.js
@@ -1,28 +1,17 @@
-import { PrismaClient } from "@prisma/client";
-import { PrismaPg } from "@prisma/adapter-pg";
-import { Pool } from "pg";
+import { PrismaClient } from '@prisma/client'
+import { PrismaPg } from '@prisma/adapter-pg'
+import { Pool } from 'pg'
 
 const prismaClientSingleton = () => {
-  const connectionString = process.env.DATABASE_URL;
-  const isLocalhost = connectionString?.includes('localhost');
-  
-  const pool = new Pool({
-    connectionString,
-    ...(isLocalhost ? {} : {
-      ssl: {
-        rejectUnauthorized: false,
-      }
-    })
-  });
-  
-  const adapter = new PrismaPg(pool);
-  return new PrismaClient({ adapter });
-};
+  const pool = new Pool({ connectionString: process.env.DATABASE_URL })
+  const adapter = new PrismaPg(pool)
+  return new PrismaClient({ adapter })
+}
 
-const globalForPrisma = globalThis;
+const globalForPrisma = globalThis
 
-const prisma = globalForPrisma.prisma ?? prismaClientSingleton();
+const prisma = globalForPrisma.prisma ?? prismaClientSingleton()
 
-export default prisma;
+export default prisma
 
-if (process.env.NODE_ENV !== "production") globalForPrisma.prisma = prisma;
+if (process.env.NODE_ENV !== 'production') globalForPrisma.prisma = prisma
